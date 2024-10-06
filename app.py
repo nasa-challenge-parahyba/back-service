984683a38adeca1d988b835890ba2905c7a7515b
Gabriel Guedes Barros da Silva <gabrielguedesbsilva@gmail.com>
2024-10-06 03:32:03 -0300

    feat: Adding root route

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from src.api.v1.api import api_router
 app = FastAPI(title='API - Nasa Challenge')
 app.include_router(api_router)
 
+@app.get("/")
+async def read_root():
+    return {"message": "Hello, World!"}
+
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8000,
                 log_level="info", reload=True)
